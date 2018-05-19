09f0900107a13aa61a3686fa2113703f9f904f5d
Steven Rolka <strolka520@gmail.com>
2018-05-19 00:21:59 -0400

    TCJSC up to S5.58

--- jsMisc/Starting S5.5/script.js
+++ jsMisc/Starting S5.5/script.js
@@ -56,7 +56,7 @@ var jane = Object.create(personProto,
 */
 
 
-
+/*
 
 //Primitives vs objects
 
@@ -98,3 +98,79 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+
+
+/////////////////////////////////////////////////////
+// Lecture: Passing functions as arguments
+
+/*
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+	var arrRes = [];
+	for (var i = 0; i < arr.length; i++) {
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+function calculateAge(el) {
+	return 2018 - el;
+}
+
+
+function isFullAge(el) {
+	return el >= 18;
+}
+
+function maxHeartRate(el) {
+	if (el >= 18 && el <= 81) {
+		return Math.round(206.9 - (0.67 * el));
+	} else {
+		return -1;
+	}
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
+
+*/
+
+
+
+////////////////////////////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+	if (job === 'designer') {
+		return function(name) {
+			console.log(name + ', can you please explain what UX design is?');
+		}
+	} else if (job === 'teacher') {
+		return function(name) {
+			console.log('What subject do you teach, ' + name + '?');
+		}
+	} else {
+		return function(name) {
+			console.log('Hello' + name + 'What do you do?');
+		}
+	}
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane');
+
+interviewQuestion('teacher')('Mark');
+
+
