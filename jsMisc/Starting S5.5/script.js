b0114299ec45979f86a7b6d9244180fca398c4af
Steven Rolka <strolka520@gmail.com>
2018-05-21 19:20:16 -0400

    Up to S5.63

--- jsMisc/Starting S5.5/script.js
+++ jsMisc/Starting S5.5/script.js
@@ -248,6 +248,7 @@ interviewQuestion('teacher')('John');
 /////////////////////////////////////////////
 // Lecture: Bind, call and apply
 
+/*
 var john = {
 	name: 'John',
 	age: 26,
@@ -308,3 +309,51 @@ var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
+*/
+
+(function () {
+	function Question(question, answers, correct) {
+		this.question = question;
+		this.answers = answers;
+		this.correct = correct;
+	}
+
+	Question.prototype.displayQuestion =
+		function() {
+		console.log(this.question);
+		for (var i = 0; i < this.answers.length; i++){
+			console.log(i + ': ' + this.answers[i]);
+		}
+	}
+
+	Question.prototype.checkAnswer = function(ans) {
+		if (ans === this.correct) {
+			console.log('Correct!');
+		} else {
+			console.log('Wrong answer, try again.');
+		}
+	}
+
+	var q1 = new Question('Is Javascript the coolest programming language?',
+		['Yes', 'No'],
+		0);
+
+	var q2 = new Question('What is the name of this course\'s teacher?', 
+		['John', 'Michael', 'Jonas'],
+		2);
+
+	var q3 = new Question('What best describes coding?',
+		['Lame', 'Fun', 'Dumb', 'Boring'],
+		1);
+
+	var questions = [q1, q2, q3];
+
+	var n = Math.floor(Math.random() * questions.length);
+
+	questions[n].displayQuestion();
+
+	var answer = parseInt(prompt('Please select the correct answer.'));
+
+	questions[n].checkAnswer(answer);
+})();
+
